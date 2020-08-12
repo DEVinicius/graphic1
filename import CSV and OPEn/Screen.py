2785781bf3b8dd0cca0c4418fa25b3c29586983c
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-12 14:27:26 -0300

    leitor de csv com arquivos

---
+++ import CSV and OPEn/Screen.py
@@ -0,0 +1,20 @@
+import tkinter
+import PySimpleGUI as sg
+
+class Screen:
+    def __init__(self):
+        #layout
+        layout = [
+            [sg.Text("Insira o Arquivo CSV")],
+            [sg.Input(key="csv_fiile"), sg.FileBrowse()],
+            [sg.Open(), sg.Cancel()]
+        ]
+
+        # Window
+        window = sg.Window("CSV OPEN", layout)
+
+        self.button, self.values = window.Read()
+
+    def Start(self):
+        csv_file_path = self.values["csv_file"]
+        print(csv_file_path)
